3a614de41d76f7d196d3fd8cd6ba5a72af07a771
nicolas-kruger <nicolas.nkruger@gmail.com>
2021-11-11 06:16:34 -0300

    first commit

---
+++ 0001game_move_function.dart
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/dart
+
+int moveArrow(int poss, int roll) => poss + 2 * roll;
+
+int move(int pos, int roll) {
+  return pos + 2 * roll;
+}
+
+void main(List<String> args) {}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Dart Code Wars
+
+my solutions of some chalenges of code wars with dart
+
+[:shipit: my code wars](https://www.codewars.com/users/nicolaskruger)
